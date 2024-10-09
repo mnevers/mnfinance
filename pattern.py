cd93b000abfbb3db95161bac2fe0dbd80223cb99
Matthew Nevers <nevers.matthew@gmail.com>
2024-10-08 20:02:08 -0400

    auto

--- pattern.py
+++ pattern.py
@@ -6,19 +6,24 @@ def get_hist_data():
     historical_data = ticker.history(start="2011-01-01", end="2024-10-04")    
     low = 0
     high = 0
+    days = 30
+    offset = 0
 
-    for cnt, val in enumerate(historical_data['Close']):        
-        if cnt == 0:
-            low = val
-            high = val
-            print(f"High: {high}")
-            print(f"Low: {low}")
-            sys.exit(0)
-
-
+    for cnt, val in enumerate(historical_data['Close']):
+        if cnt > days:
+            for n in range(days):
+                if n == 0:
+                    low = historical_data['Close'].iloc[n+offset]
+                    high = historical_data['Close'].iloc[n+offset]
+                else:
+                    if historical_data['Close'].iloc[n+offset] < low:
+                        low = historical_data['Close'].iloc[n+offset]
+                    if historical_data['Close'].iloc[n+offset] > high:
+                        high = historical_data['Close'].iloc[n+offset]
+            offset+=1
+           
     print(f"High: {high}")
     print(f"Low: {low}")
-        
 
 
 if __name__ == "__main__":
