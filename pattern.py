6052c589a6d08b237fa4d81b93fa56e10b71f5d9
Matthew Nevers <nevers.matthew@gmail.com>
2024-10-08 21:45:42 -0400

    auto

--- pattern.py
+++ pattern.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     for p in range(59):
         for f in range(59):
             r = process_data(p+1,f+1)    
-            print(r)
+            print(f"{p+1},{f+1}")
             #time.sleep(.1)  # waiting so we don't overload API      
             percent = r[0] + r[1]
             percent = r[0]/percent
