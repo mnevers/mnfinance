becbe4f9f2f63730da4889e430ed11a78e040775
Matthew Nevers <nevers.matthew@gmail.com>
2024-10-09 21:24:38 -0400

    auto

--- lowbuy.py
+++ lowbuy.py
@@ -4,18 +4,18 @@ import sys
 
 def get_data(tic):
     ticker = yf.Ticker(tic)
-    historical_data = ticker.history(start="2010-01-01", end="2024-10-04")
+    historical_data = ticker.history(start="2011-01-01", end="2024-10-04")
     print(len(historical_data))
 
     return historical_data
 
 def process_data(past,future,historical_data):        
+    low_days = 0
+    high_days = 0    
     low = 0
     high = 0
 
     offset = 0
-    low_days = 0
-    high_days = 0    
 
     for cnt, val in enumerate(historical_data['Close']):       
         if cnt > past:#if we have enough hist data to process
@@ -31,49 +31,27 @@ def process_data(past,future,historical_data):
             offset+=1 #estabilish offset for n loop
             if val < low: #if at N time high or low
                 if cnt+future < len(historical_data): #if not index greater that data set
-                    if historical_data['Close'].iloc[cnt+future] > val: #if F closing price is higher
+                    temp = historical_data['Close'].iloc[cnt+future] - val
+                    per = temp / val
+                    if per > .1: #if F closing price is higher
                         high_days+=1
-                    elif historical_data['Close'].iloc[cnt+future] < val:#if f closing price lower
+                    else:
                         low_days+=1
+                        
+                    
             
-    #print(f"{past} past days and {future} days in future")
-    #print(f"High: {high_days}")
-    #print(f"Low: {low_days}")
     return low_days,high_days
-
 
 if __name__ == "__main__":    
     tic = "UPRO"
     hist = get_data(tic)
-    
-    best = 0
-    best_p = 0
-    best_f = 0
-    percent = 0    
 
-    #60 is 3 months 20 business days in month about
-    for p in range(89):
-        for f in range(29):
-            r = process_data(p+1,f+1,hist)    
-            print(f"{p+1},{f+1}")
-            #time.sleep(.3)  # waiting so we don't overload API      
-            percent = r[0] + r[1]
-            percent = r[1]/percent
+    r = process_data(1,10,hist)
+    t = r[0] + r [1]
+    close_percent = r[0] / t
 
-            if best < percent:
-                best = percent
-                best_p = p+1
-                best_f = f+1
-                print(f"Best Past: {best_p} Best Future: {best_f} Best Percent: {best}")
-    
-    
-    out = tic + " Best Past days: " + str(best_p) + ", Best future days: " + str(best_f) + ". With a percentage of: " + str(best) + "\n"
+    out = tic + " closed higher by .1 or more percentage of time: " + str(close_percent) + "\n"
     print(out)
-
-    text_file = open("Output.txt", "w")   
-    
-    text_file.write(out)
-    text_file.close()
 
 
     
